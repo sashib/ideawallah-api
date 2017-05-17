9942a714f08bf2b6acfbef704d8a305245805df1
Sashi <sbommakanty@kahuna.com>
2017-05-16 18:38:30 -0700

    added some azure stuff

--- db.js
+++ db.js
@@ -8,7 +8,8 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 var mongoURI = {
-  prod: 'mongodb://db:ideasd3v@ds021691.mlab.com:21691/ideasdb',
+  //production: 'mongodb://db:ideasd3v@ds021691.mlab.com:21691/ideasdb',
+  production: 'mongodb://ideawallahdb:LZfikYbGjBdQksTlYp7f4v5SjIHN3GwAEkQX2ED0BNKvPlw9jpzA9rsfPIhBNHGPALJ7R7iqlzmQ1Zc9LPSyjw==@ideawallahdb.documents.azure.com:10250/mean?ssl=true&sslverifycertificate=false',
   dev: 'mongodb://dbdev:dbdev@ds137291.mlab.com:37291/ideasdbdev',
   test: 'mongodb://dbtest:dbtest@ds139761.mlab.com:39761/ideasdbtest'
 };
